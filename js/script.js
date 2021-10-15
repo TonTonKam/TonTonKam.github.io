d8205c0bde34a0e405b2acb878bf3ff70a377765
TonTonKam <yannkrz@hotmail.com>
2021-10-15 13:42:35 +0200

    ajout de la partie cssVide

--- js/script.js
+++ js/script.js
@@ -46,28 +46,16 @@ menu1.onmouseout = displayOff;
 // menu1.addEventListener("onmouseover", displayOn, false);
 // menu1.addEventListener("onmouseout", displayOff, false);
 
+//paragraphe
+let paraDesc = document.getElementById('paraDescription');
+let paraMotiv = document.getElementById('paraMotivations');
+let paraParc = document.getElementById('paraParcours');
+let paraComp = document.getElementById('paraCompetences');
 
 /**
  * j'ai créé un switch de passage de titre pour que quand l'on clic
  * sur un bouton ou une classe, on puisse changer de page virtuellement
  */
-function clicker(evt){
-    // /**
-    //  * dans cette fonction, le 1er <li> prend '2em', mais les autres
-    //  * aussi prennent aussi '2em' se qui rends les 2e <li> a '4em'
-    //  */
-    // evt.target.style.fontSize = '2em';
-
-    //permet de mettre l'event que sur 1 element
-    evt.stopPropagation();
-    tar0 = evt.target;
-    tar = evt.target.id;
-    tar1 = evt.target.class;
-    console.log(evt);
-    console.log("valeur de tar0 : " + tar0);
-    console.log("valeur de tar : " + tar);
-    console.log("valeur de tar1 : " + tar1);
-}
 
 /**
  * la fonction prend comme evt l'endroit cliqué
@@ -104,4 +92,87 @@ function modifiTitle(nameValue){
  * j'ai bien envie de faire un switch pour le contenu aussi
  * mais "mes compétences / réalisations" auront des liens et des images
  */
+
+/********************* Ajout de contenance de cssVide *********************/
+
+//*****presentation par default*****
+titleSelector.textContent = 'En se qui me concerne';
+funcNone(paraMotiv);
+funcNone(paraParc);
+funcNone(paraComp);
+funcOn(paraDesc);
+
+function clicker(evt){
+    evt.stopPropagation();
+    changeTitle(evt.target.id);
+}
+
+function changeTitle(nameValue){
+    // switch(nameValue){
+    //     case 'accueil':
+    //         titleSelector.textContent = 'A propos de moi';
+    //         break;
+    //     case 'motivations':
+    //         titleSelector.textContent = 'Mes motivations';
+    //         break;
+    //     case 'parcours':
+    //         titleSelector.textContent = 'Mon parcours professionnel';
+    //         break;
+    //     case 'competences':
+    //         titleSelector.textContent = 'Mes compétences / réalisations';
+    //         break;
+    //     default:
+    //         //accueil
+    //         titleSelector.textContent = 'A propos de moi def';
+    //         break;
+    // }
+    if(nameValue == 'apropos'){
+        titleSelector.textContent = 'En se qui me concerne';
+        funcNone(paraMotiv);
+        funcNone(paraParc);
+        funcNone(paraComp);
+        funcOn(paraDesc);
+    }
+    else if(nameValue == 'motivations'){
+        titleSelector.textContent = 'Mes motivations';
+        funcNone(paraDesc);
+        funcNone(paraParc);
+        funcNone(paraComp);
+        funcOn(paraMotiv);
+    }
+    else if(nameValue == 'parcours'){
+        titleSelector.textContent = 'Mon parcours professionnel';
+        funcNone(paraMotiv);
+        funcNone(paraDesc);
+        funcNone(paraComp);
+        funcOn(paraParc);
+    }
+    else if(nameValue == 'competences'){
+        titleSelector.textContent = 'Mes compétences / réalisations';
+        funcNone(paraMotiv);
+        funcNone(paraDesc);
+        funcNone(paraParc);
+        funcOn(paraComp);
+    }
+    else{
+        titleSelector.textContent = 'A propos de moi';
+    }
+}
+
+/**
+ * j'ai bien envie de faire un switch pour le contenu aussi
+ * mais "mes compétences / réalisations" auront des liens et des images
+ */
+
+/**
+ * 
+ * @param {*} doc permet de prendre la div ciblé et de lui attribuer un display: none;
+ */
+function funcNone(docs){
+    //fonction avec les div pas avec les p
+    docs.style.display = 'none';
+}
+function funcOn(doc) {
+    doc.style.display = 'inline-block';
+}
 
